1e3cb053c6a35b8ab6b8dec1c0067c222ff87db3
quyen-luu <luunguyenthaoquyen@gmail.com>
2025-05-30 23:40:17 +0700

    Add
    Content

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        //Khai bao mang
+        string[] students = {
+            "Christian", "Michael",
+            "Camila", "Sienna",
+            "Tanya", "Connor",
+            "Zachariah"
+            };
+        //Khai bao bien luu ten tim kiem
+        Console.Write("Enter a student's name: ");
+        string inputName = Console.ReadLine();
+
+        //Duyet mang
+        bool isExist = false;
+        for (int i = 0; i < students.Length; i++)
+        {
+            if (students[i].Equals(inputName))
+            {
+                Console.WriteLine($"Position of the student in the list {inputName} " + (i + 1));
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist)
+        {
+            Console.WriteLine($"Student {inputName} was not found in the list.");
+        }
+    }
+}
